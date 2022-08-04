5b8548fb36f709982c6d779a3c91653906a006d3
Mohamed Moustafa <mti.moustafa@gmail.com>
2022-08-04 13:04:08 +0200

    Attempt to host static resume through node

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-import './main.css'
+import express from 'express'
+import { dirname, resolve } from 'path'
+
+const app = express()
+const port = process.env.PORT ?? 3000
+
+app.use(express.static('dist'))
+
+app.get('/', (request, response) => {
+  response.sendFile(resolve('dist/index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Serving resume on port ${port}`)
+})
