b85225be3b5e591068fd84bc0ffe164257db10ad
Mohamed Moustafa <hi@fairewinds.ca>
2025-03-28 12:21:06 +0100

    Polish resume for final draft
    * Update work experience copy
    * Update skills & interests layout
    * Update skills, interests, tech stack

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default {
       helpers: {
         emailLink: email => `mailto:${email}`,
         slugify: value => value?.toLowerCase().replaceAll(/\s+/g, '-') || '',
+        listify: list => list.map(item => item.name).reduce((acc, val) => acc + '; ' + val),
       },
       partialDirectory: [
         partialsBasePath,
